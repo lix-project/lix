32cb862f987a61c87cc6a32d1437031941875144
Jörg Thalheim <joerg@thalheim.io>
2021-03-15 06:09:41 +0100

    add ci configuration

---
+++ hydra.nix
@@ -0,0 +1,35 @@
+{ stdenv
+, nixFlakes
+, meson
+, cmake
+, ninja
+, pkg-config
+, boost
+, nlohmann_json
+, srcDir ? ./.
+}:
+
+let
+  filterMesonBuild = dir: builtins.filterSource
+    (path: type: type != "directory" || baseNameOf path != "build") dir;
+in
+stdenv.mkDerivation rec {
+  pname = "hydra-eval-jobs";
+  version = "0.0.1";
+  src = filterMesonBuild srcDir;
+  buildInputs = [
+    nlohmann_json nixFlakes boost
+  ];
+  nativeBuildInputs = [
+    meson pkg-config ninja
+    # nlohmann_json can be only discovered via cmake files
+    cmake
+  ];
+  meta = with stdenv.lib; {
+    description = "Hydra's builtin hydra-eval-jobs as a standalone";
+    homepage = "https://github.com/Mic92/hydra-eval-jobs";
+    license = licenses.mit;
+    maintainers = with maintainers; [ mic92 ];
+    platforms = platforms.unix;
+  };
+}
