76c3e68ccd369c0a788d6a779012d75248ffe96c
Jörg Thalheim <joerg@thalheim.io>
2021-03-21 19:18:03 +0100

    run tests in CI

--- tests/assets/ci.nix
+++ tests/assets/ci.nix
@@ -1,4 +1,6 @@
-with import <nixpkgs> {};
+let
+  pkgs = import (builtins.getFlake (toString ./.)).inputs.nixpkgs {};
+in
 {
   builtJob = pkgs.writeText "job1" "job1";
   substitutedJob = pkgs.hello;
