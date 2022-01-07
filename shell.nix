44655761c5e524814be760e19b6f024d379445b9
adisbladis <adisbladis@gmail.com>
2022-01-08 08:53:26 +1300

    Use locked nixpkgs version for nix-shell

--- shell.nix
+++ shell.nix
@@ -1,4 +1,15 @@
-{ pkgs ? import <nixpkgs> { }
+{ pkgs ? (
+    let
+      inherit (builtins) fromJSON readFile;
+      flakeLock = fromJSON (readFile ./flake.lock);
+      locked = flakeLock.nodes.nixpkgs.locked;
+      nixpkgs = assert locked.type == "github"; builtins.fetchTarball {
+        url = "https://github.com/${locked.owner}/${locked.repo}/archive/${locked.rev}.tar.gz";
+        sha256 = locked.narHash;
+      };
+    in
+    import nixpkgs { }
+  )
 , srcDir ? null
 }:
 
