0f6ac00feaf6da743b5f023964f4fbbeb04724b9
adisbladis <adisbladis@gmail.com>
2022-01-08 09:32:53 +1300

    Use the NixOS unstable channel

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Hydra's builtin hydra-eval-jobs as a standalone";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
