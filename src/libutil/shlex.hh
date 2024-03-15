aee3d639b5096349413021537ae842c8c33ef6cf
Rebecca Turner <rbt@sent.as>
2024-03-14 17:44:43 -0700

    Move `shell_words` into its own file
    Change-Id: I34c0ebfb6dcea49bf632d8880e04075335a132bf

---
+++ src/libutil/shlex.hh
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <regex>
+#include <string>
+#include <vector>
+
+#include "error.hh"
+
+namespace nix {
+
+class ShlexError : public Error
+{
+public:
+    const std::string input;
+
+    ShlexError(const std::string input)
+        : Error("Failed to parse shell arguments (unterminated quote?): %1%", input)
+        , input(input)
+    {
+    }
+};
+
+/**
+ * Parse a string into shell arguments.
+ *
+ * Takes care of whitespace, quotes, and backslashes (at least a bit).
+ */
+std::vector<std::string> shell_split(const std::string & input);
+
+} // namespace nix
