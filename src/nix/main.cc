3d065192c08b3dcb76b9cf70cd0104974106ae8f
John Ericson <John.Ericson@Obsidian.Systems>
2024-03-18 20:23:20 -0600

    Overhaul completions, redo #6693 (#8131)
    As I complained in
    https://github.com/NixOS/nix/pull/6784#issuecomment-1421777030 (a
    comment on the wrong PR, sorry again!), #6693 introduced a second
    completions mechanism to fix a bug. Having two completion mechanisms
    isn't so nice.
    
    As @thufschmitt also pointed out, it was a bummer to go from `FlakeRef`
    to `std::string` when collecting flake refs. Now it is `FlakeRefs`
    again.
    
    The underlying issue that sought to work around was that completion of
    arguments not at the end can still benefit from the information from
    latter arguments.
    
    To fix this better, we rip out that change and simply defer all
    completion processing until after all the (regular, already-complete)
    arguments have been passed.
    
    In addition, I noticed the original completion logic used some global
    variables. I do not like global variables, because even if they save
    lines of code, they also obfuscate the architecture of the code.
    
    I got rid of them  moved them to a new `RootArgs` class, which now has
    `parseCmdline` instead of `Args`. The idea is that we have many argument
    parsers from subcommands and what-not, but only one root args that owns
    the other per actual parsing invocation. The state that was global is
    now part of the root args instead.
    
    This did, admittedly, add a bunch of new code. And I do feel bad about
    that. So I went and added a lot of API docs to try to at least make the
    current state of things clear to the next person.
    
    --
    
    This is needed for RFC 134 (tracking issue #7868). It was very hard to
    modularize `Installable` parsing when there were two completion
    arguments. I wouldn't go as far as to say it is *easy* now, but at least
    it is less hard (and the completions test finally passed).
    
    Co-authored-by: Valentin Gagarin <valentin.gagarin@tweag.io>
    Change-Id: If18cd5be78da4a70635e3fdcac6326dbfeea71a5
    (cherry picked from commit 67eb37c1d0de28160cd25376e51d1ec1b1c8305b)

--- src/nix/main.cc
+++ src/nix/main.cc
@@ -1,5 +1,6 @@
 #include <algorithm>
 
+#include "args/root.hh"
 #include "command.hh"
 #include "common-args.hh"
 #include "eval.hh"
@@ -75,7 +76,7 @@ static bool haveInternet()
 
 std::string programPath;
 
-struct NixArgs : virtual MultiCommand, virtual MixCommonArgs
+struct NixArgs : virtual MultiCommand, virtual MixCommonArgs, virtual RootArgs
 {
     bool useNet = true;
     bool refresh = false;
@@ -253,10 +254,7 @@ static void showHelp(std::vector<std::string> subcommand, NixArgs & toplevel)
 
 static NixArgs & getNixArgs(Command & cmd)
 {
-    assert(cmd.parent);
-    MultiCommand * toplevel = cmd.parent;
-    while (toplevel->parent) toplevel = toplevel->parent;
-    return dynamic_cast<NixArgs &>(*toplevel);
+    return dynamic_cast<NixArgs &>(cmd.getRoot());
 }
 
 struct CmdHelp : Command
@@ -424,16 +422,16 @@ void mainWrapped(int argc, char * * argv)
 
     Finally printCompletions([&]()
     {
-        if (completions) {
-            switch (completionType) {
-            case ctNormal:
+        if (args.completions) {
+            switch (args.completions->type) {
+            case Completions::Type::Normal:
                 logger->cout("normal"); break;
-            case ctFilenames:
+            case Completions::Type::Filenames:
                 logger->cout("filenames"); break;
-            case ctAttrs:
+            case Completions::Type::Attrs:
                 logger->cout("attrs"); break;
             }
-            for (auto & s : *completions)
+            for (auto & s : args.completions->completions)
                 logger->cout(s.completion + "\t" + trim(s.description));
         }
     });
@@ -441,7 +439,7 @@ void mainWrapped(int argc, char * * argv)
     try {
         args.parseCmdline(argvToStrings(argc, argv));
     } catch (UsageError &) {
-        if (!args.helpRequested && !completions) throw;
+        if (!args.helpRequested && !args.completions) throw;
     }
 
     if (args.helpRequested) {
@@ -458,10 +456,7 @@ void mainWrapped(int argc, char * * argv)
         return;
     }
 
-    if (completions) {
-        args.completionHook();
-        return;
-    }
+    if (args.completions) return;
 
     if (args.showVersion) {
         printVersion(programName);
