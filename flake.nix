655dae03deaba8b74076230119b211ca19757b5d
Rebecca Turner <rbt@sent.as>
2024-03-27 12:28:44 -0700

    Add `treefmt` `pre-commit` hook
    This lets us use different formatters for different filetypes.
    
    Change-Id: Ib52383dd5097c8919a65e299aca2b5a55412223c

--- flake.nix
+++ flake.nix
@@ -331,6 +331,10 @@
               stages = [ "commit" ];
               excludes = [ "^tests/functional/lang/" ];
             };
+            treefmt = {
+              enable = true;
+              settings.formatters = [ ];
+            };
           };
         }) pre-commit-hooks.lib;
       };
