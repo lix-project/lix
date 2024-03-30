43cf487c256395e03f2e10f1f66b3933f201a4e8
Jade Lovelace <lix@jade.fyi>
2024-03-29 20:26:38 -0700

    Create clang-tidy check to rename all our includes
    It is a little bit scuffed, but it seems to produce correct results. We
    can run it at a later date when we want to explode every in-flight
    commit in existence and then need to filter-branch them.
    
    Fixes: https://git.lix.systems/lix-project/lix/issues/188
    
    Change-Id: Id97e4651f78804a941d941df02c7c1b21ce453b6

--- clang-tidy/LixClangTidyChecks.cc
+++ clang-tidy/LixClangTidyChecks.cc
@@ -1,5 +1,6 @@
 #include <clang-tidy/ClangTidyModule.h>
 #include <clang-tidy/ClangTidyModuleRegistry.h>
+#include "FixIncludes.hh"
 #include "HasPrefixSuffix.hh"
 
 namespace nix::clang_tidy {
@@ -9,9 +10,10 @@ using namespace clang::tidy;
 class NixClangTidyChecks : public ClangTidyModule {
     public:
         void addCheckFactories(ClangTidyCheckFactories &CheckFactories) override {
-            CheckFactories.registerCheck<HasPrefixSuffixCheck>("nix-hasprefixsuffix");
+            CheckFactories.registerCheck<HasPrefixSuffixCheck>("lix-hasprefixsuffix");
+            CheckFactories.registerCheck<FixIncludesCheck>("lix-fixincludes");
         }
 };
 
-static ClangTidyModuleRegistry::Add<NixClangTidyChecks> X("nix-module", "Adds nix specific checks");
+static ClangTidyModuleRegistry::Add<NixClangTidyChecks> X("lix-module", "Adds lix specific checks");
 };
