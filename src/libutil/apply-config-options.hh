c300efc0e1ff021c2afa68ea1e470f55d31e3718
Rebecca Turner <rbt@sent.as>
2024-08-23 16:59:38 -0700

    Add `ApplyConfigOptions`
    Change-Id: Ic876bcabd0b68e579bbd30ca1755919df43d4813

---
+++ src/libutil/apply-config-options.hh
@@ -0,0 +1,53 @@
+#pragma once
+/**
+ * @file
+ * @brief Options for applying `Config` settings.
+ */
+
+#include <types.hh>
+
+namespace nix {
+
+/**
+ * Options for applying `Config` settings.
+ */
+struct ApplyConfigOptions
+{
+    /**
+     * The configuration file being loaded.
+     *
+     * If set, relative paths are allowed and interpreted as relative to the
+     * directory of this path.
+     */
+    std::optional<Path> path = std::nullopt;
+
+    /**
+     * If set, tilde paths (like `~/.config/repl.nix`) are allowed and the
+     * tilde is substituted for this directory.
+     */
+    std::optional<Path> home = std::nullopt;
+
+    /**
+     * Is the configuration being loaded from the `$NIX_CONFIG` environment
+     * variable?
+     *
+     * Used for formatting error messages.
+     */
+    bool fromEnvVar = false;
+
+    /**
+     * Display the `relative` path field, with a reasonable default if none is
+     * available.
+     */
+    std::string relativeDisplay() const {
+        if (path) {
+            return *path;
+        } else if (fromEnvVar) {
+            return "$NIX_CONFIG";
+        } else {
+            return "<unknown>";
+        }
+    }
+};
+
+}
