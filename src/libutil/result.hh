92eccfbd68f80e4a5d87556d43927d6dbaabae26
eldritch horrors <pennae@lix.systems>
2024-09-01 01:37:10 +0200

    libutil: add a result type using boost outcome
    we're using boost::outcome rather than leaf or stl types because stl
    types are not available everywhere and leaf does not provide its own
    storage for error values, relying on thread-locals and the stack. if
    we want to use promises we won't have a stack and would have to wrap
    everything into leaf-specific allocating wrappers, so outcome it is.
    
    Change-Id: I35111a1f9ed517e7f12a839e2162b1ba6a993f8f

---
+++ src/libutil/result.hh
@@ -0,0 +1,24 @@
+#pragma once
+/// @file
+
+#include <boost/outcome/std_outcome.hpp>
+#include <boost/outcome/std_result.hpp>
+#include <boost/outcome/success_failure.hpp>
+#include <exception>
+
+namespace nix {
+
+template<typename T, typename E = std::exception_ptr>
+using Result = boost::outcome_v2::std_result<T, E>;
+
+template<typename T, typename D, typename E = std::exception_ptr>
+using Outcome = boost::outcome_v2::std_outcome<T, D, E>;
+
+namespace result {
+
+using boost::outcome_v2::success;
+using boost::outcome_v2::failure;
+
+}
+
+}
