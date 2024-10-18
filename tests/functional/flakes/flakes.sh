22eb47f0fd58122d9e429ebd39d61feea0b5e42f
Dusk Banks <me@bb010g.com>
2024-10-18 13:44:49 -0700

    tests/functional/flakes: test with UTF-8 bullets
    using UTF-8 bullets in the sample avoids locale confusion where Bash
    doesn't know to treat `•` as a single character.
    
    Signed-off-by: Dusk Banks <me@bb010g.com>
    Change-Id: I829019b66e93e6d33ac3a6641df07d0dd2332a5a

--- tests/functional/flakes/flakes.sh
+++ tests/functional/flakes/flakes.sh
@@ -285,11 +285,11 @@ nix build -o $TEST_ROOT/result $flake3Dir#sth --commit-lock-file
 
 Flake lock file updates:
 
-"?" Added input 'nonFlake':
+• Added input 'nonFlake':
     'git+file://"*"/flakes/flakes/nonFlake?ref=refs/heads/master&rev="*"' "*"
-"?" Added input 'nonFlakeFile':
+• Added input 'nonFlakeFile':
     'path:"*"/flakes/flakes/nonFlake/README.md?lastModified="*"&narHash=sha256-cPh6hp48IOdRxVV3xGd0PDgSxgzj5N/2cK0rMPNaR4o%3D' "*"
-"?" Added input 'nonFlakeFile2':
+• Added input 'nonFlakeFile2':
     'path:"*"/flakes/flakes/nonFlake/README.md?lastModified="*"&narHash=sha256-cPh6hp48IOdRxVV3xGd0PDgSxgzj5N/2cK0rMPNaR4o%3D' "* ]]
 
 nix build -o $TEST_ROOT/result flake3#fnord
