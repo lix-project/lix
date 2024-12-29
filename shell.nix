5c7ea4f446de58aa64f78087bb4ec26b9c4111aa
Chip Bilbrey <chip.bilbrey@gmail.com>
2024-12-29 14:15:16 -0800

    Use locked flake-compat in shell.nix
    Change-Id: I059aa6da6365f1a5a46b506e57d9b3ab2b73d8d8

--- shell.nix
+++ shell.nix
@@ -1,7 +1,9 @@
 let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
   shell =
-    (import (fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz") {
-      src = ./.;
-    }).shellNix;
+    (import (fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }) { src = ./.; }).shellNix;
 in
 shell.default // shell.devShells.${builtins.currentSystem}
