9760c005915f8c16564a0a9db2b346b197d6ecf5
Jade Lovelace <lix@jade.fyi>
2025-01-15 23:26:07 -0800

    clang-tidy: Obliterate FixIncludes check
    We have obliterated the support across the build system for these
    include paths anywhere, so the codemod is now redundant and can go.
    
    Change-Id: I54082f39752c6aac6429e1c24026211adae8221a

--- subprojects/lix-clang-tidy/LixClangTidyChecks.cc
+++ subprojects/lix-clang-tidy/LixClangTidyChecks.cc
@@ -1,6 +1,5 @@
 #include <clang-tidy/ClangTidyModule.h>
 #include <clang-tidy/ClangTidyModuleRegistry.h>
-#include "FixIncludes.hh"
 #include "HasPrefixSuffix.hh"
 #include "CharPtrCast.hh"
 
@@ -12,7 +11,6 @@ class NixClangTidyChecks : public ClangTidyModule {
     public:
         void addCheckFactories(ClangTidyCheckFactories &CheckFactories) override {
             CheckFactories.registerCheck<HasPrefixSuffixCheck>("lix-hasprefixsuffix");
-            CheckFactories.registerCheck<FixIncludesCheck>("lix-fixincludes");
             CheckFactories.registerCheck<CharPtrCastCheck>("lix-charptrcast");
         }
 };
