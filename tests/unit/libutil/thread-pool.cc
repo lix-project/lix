lix-project/lix

commit 4138fc7622bcd23613050073864076591b76d519

libutil: don't do work on threadpool main thread
this is mandatory for async support in worker threads at this time.

Change-Id: Ie58871bf33f6266aec58f37e10cfbc47f82a3aa9

lix/libutil/thread-pool.cc
@@ -2,6 +2,7 @@
 #include "lix/libutil/logging.hh"
 #include "lix/libutil/signals.hh"
 #include "lix/libutil/thread-name.hh"
+#include <kj/common.h>
 
 namespace nix {
 
@@ -13,7 +14,7 @@ ThreadPool::ThreadPool(const char * name, size_t _maxThreads)
         if (!maxThreads) maxThreads = 1;
     }
 
-    debug("starting pool of %d threads", maxThreads - 1);
+    debug("starting pool of %d threads", maxThreads);
 }
 
 ThreadPool::~ThreadPool()
@@ -46,9 +47,8 @@ void ThreadPool::enqueue(const work_t & t)
     if (quit)
         throw ThreadPoolShutDown("cannot enqueue a work item while the thread pool is shutting down");
     state->pending.push(t);
-    /* Note: process() also executes items, so count it as a worker. */
-    if (state->pending.size() > state->workers.size() + 1 && state->workers.size() + 1 < maxThreads)
-        state->workers.emplace_back(&ThreadPool::doWork, this, false);
+    if (state->pending.size() > state->workers.size() && state->workers.size() < maxThreads)
+        state->workers.emplace_back(&ThreadPool::doWork, this);
     work.notify_one();
 }
 
@@ -56,14 +56,15 @@ void ThreadPool::process()
 {
     state_.lock()->draining = true;
 
-    /* Do work until no more work is pending or active. */
+    /* Wait until no more work is pending or active. */
     try {
-        doWork(true);
+        if (auto state(state_.lock()); state->active == 0 && state->pending.empty()) {
+            return;
+        }
+
+        quit.wait(false);
 
         auto state(state_.lock());
-
-        assert(quit);
-
         if (state->exception)
             std::rethrow_exception(state->exception);
 
@@ -78,14 +79,18 @@ void ThreadPool::process()
     }
 }
 
-void ThreadPool::doWork(bool mainThread)
+void ThreadPool::doWork()
 {
+    // Tell the other workers to quit; we only return on errors or completion
+    KJ_DEFER({
+        quit = true;
+        quit.notify_all();
+        work.notify_all();
+    });
     ReceiveInterrupts receiveInterrupts;
 
-    if (!mainThread) {
-        setCurrentThreadName(this->name);
-        interruptCheck = [&]() { return (bool) quit; };
-    }
+    setCurrentThreadName(this->name);
+    interruptCheck = [&]() { return (bool) quit; };
 
     bool didWork = false;
     std::exception_ptr exc;
@@ -103,9 +108,7 @@ void ThreadPool::doWork(bool mainThread)
 
                     if (!state->exception) {
                         state->exception = exc;
-                        // Tell the other workers to quit.
-                        quit = true;
-                        work.notify_all();
+                        return;
                     } else {
                         /* Print the exception, since we can't
                            propagate it. */
@@ -136,16 +139,12 @@ void ThreadPool::doWork(bool mainThread)
             /* Wait until a work item is available or we're asked to
                quit. */
             while (true) {
-                if (quit) return;
-
                 if (!state->pending.empty()) break;
 
                 /* If there are no active or pending items, and the
                    main thread is running process(), then no new items
                    can be added. So exit. */
                 if (!state->active && state->draining) {
-                    quit = true;
-                    work.notify_all();
                     return;
                 }
 

lix/libutil/thread-pool.hh
@@ -73,7 +73,7 @@ private:
 
     std::condition_variable work;
 
-    void doWork(bool mainThread);
+    void doWork();
 
     void shutdown();
 };

commit e47bff547ac00b0ca12f2d370c0538397f8c3e6f

libutil: fix some thread pool bugs
4138fc7622bcd23613050073864076591b76d519 mistakenly removed an early
exit from non-main worker threads. this led to exceptions not ending
`process()` calls in a timely manner and instead draining the entire
work queue first, which for e.g. Interrupted errors would cause many
duplicated reports per worker thread instead of only one per thread.

it also did not properly rethrow a work item exception in all cases,
e.g. when all work had completed by the time `process()` was called.

due to a mistake in the thread starting check it was possible that a
system would require n² work items to start n worker threads if some
work items process quickly enough while other items block for a bit.

Change-Id: I7d59483580cda1c19980f9296074216a0fbf2c4e

lix/libutil/thread-pool.cc
@@ -47,22 +47,24 @@ void ThreadPool::enqueueWithAio(const work_t & t)
     if (quit)
         throw ThreadPoolShutDown("cannot enqueue a work item while the thread pool is shutting down");
     state->pending.push(t);
-    if (state->pending.size() > state->workers.size() && state->workers.size() < maxThreads)
+    if (state->active == state->workers.size() && state->workers.size() < maxThreads)
         state->workers.emplace_back(&ThreadPool::doWork, this);
     work.notify_one();
 }
 
 void ThreadPool::process()
 {
-    state_.lock()->draining = true;
+    const auto shouldWait = [&] {
+        auto state(state_.lock());
+        state->draining = true;
+        return state->active > 0 || !state->pending.empty();
+    }();
 
     /* Wait until no more work is pending or active. */
     try {
-        if (auto state(state_.lock()); state->active == 0 && state->pending.empty()) {
-            return;
+        if (shouldWait) {
+            quit.wait(false);
         }
-
-        quit.wait(false);
 
         auto state(state_.lock());
         if (state->exception)
@@ -141,6 +143,8 @@ void ThreadPool::doWork()
             /* Wait until a work item is available or we're asked to
                quit. */
             while (true) {
+                if (quit) return;
+
                 if (!state->pending.empty()) break;
 
                 /* If there are no active or pending items, and the

tests/unit/libutil/thread-pool.cc
@@ -1,0 +1,111 @@
+#include "lix/libutil/thread-pool.hh"
+#include <atomic>
+#include <exception>
+#include <gtest/gtest.h>
+#include <memory>
+
+static auto onThreadExit(auto fn)
+{
+    auto deferred = kj::defer(std::move(fn));
+    return std::make_shared<decltype(deferred)>(std::move(deferred));
+}
+
+namespace nix {
+
+TEST(ThreadPool, creates_threads)
+{
+    ThreadPool t{"test", 2};
+
+    std::atomic_bool unblockA{false}, unblockB{false};
+    std::atomic_bool started{false};
+
+    t.enqueue([&] {
+        started = true;
+        started.notify_all();
+        unblockA.wait(false);
+        unblockB = true;
+        unblockB.notify_all();
+    });
+    started.wait(false);
+
+    // now no work is pending. the next enqueue should start a
+    // new thread; if it does not we'll deadlock and time out.
+
+    started = false;
+    t.enqueue([&] {
+        started = true;
+        started.notify_all();
+        unblockB.wait(false);
+    });
+    started.wait(false);
+
+    unblockA = true;
+    unblockA.notify_all();
+
+    t.process();
+}
+
+TEST(ThreadPool, early_quit)
+{
+    ThreadPool t{"test", 2};
+    bool ran_anyway = false;
+
+    struct Dead : std::exception {};
+
+    std::atomic_bool unblockA{false}, unblockB{false};
+    std::atomic_bool started{false};
+
+    t.enqueue([&] {
+        started = true;
+        started.notify_all();
+        unblockA.wait(false);
+        thread_local auto _ = onThreadExit([&] {
+            unblockB = true;
+            unblockB.notify_all();
+        });
+        throw Dead{};
+    });
+    started.wait(false);
+
+    started = false;
+    t.enqueue([&] {
+        started = true;
+        started.notify_all();
+        unblockB.wait(false);
+    });
+    started.wait(false);
+
+    // this one should never run. the first thread saw an exception,
+    // and the second thread should have exited early because of it.
+    t.enqueue([&] { ran_anyway = true; });
+
+    unblockA = true;
+    unblockA.notify_all();
+
+    ASSERT_THROW(t.process(), Dead);
+    ASSERT_FALSE(ran_anyway);
+}
+
+TEST(ThreadPool, always_rethrows)
+{
+    ThreadPool t{"test"};
+
+    struct Dead : std::exception {};
+
+    std::atomic_bool flag{false};
+
+    t.enqueue([&] {
+        thread_local auto _ = onThreadExit([&] {
+            flag = true;
+            flag.notify_all();
+        });
+
+        throw Dead{};
+    });
+
+    flag.wait(false);
+
+    ASSERT_THROW(t.process(), Dead);
+}
+
+}

tests/unit/meson.build
@@ -64,6 +64,7 @@ libutil_tests_sources = files(
   'libutil/serialise.cc',
   'libutil/suggestions.cc',
   'libutil/tests.cc',
+  'libutil/thread-pool.cc',
   'libutil/url.cc',
   'libutil/url-name.cc',
   'libutil/xml-writer.cc',