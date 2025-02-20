f06040f5d883798f1492bed010220bda443ae3b4
eldritch horrors <pennae@lix.systems>
2025-02-21 00:35:11 +0100

    libutil: add computeClosureAsync
    Change-Id: I5d9a11a106e9e0d9e49d08d1d04f579ba55255a9

--- lix/libutil/closure.hh
+++ lix/libutil/closure.hh
@@ -1,7 +1,10 @@
 #pragma once
 ///@file
 
+#include "lix/libutil/async.hh"
+#include "lix/libutil/result.hh"
 #include <functional>
+#include <kj/async.h>
 #include <set>
 
 namespace nix {
@@ -27,6 +30,31 @@ std::set<T> computeClosure(
     }
 
     return res;
+}
+
+template<typename T>
+kj::Promise<Result<std::set<T>>> computeClosureAsync(
+    std::set<T> startElts,
+    std::function<kj::Promise<Result<std::set<T>>>(const T &)> getEdges
+)
+try {
+    std::set<T> res, queue = std::move(startElts);
+
+    while (!queue.empty()) {
+        std::set<T> next;
+
+        for (auto & e : queue) {
+            if (res.insert(e).second) {
+                next.merge(LIX_TRY_AWAIT(getEdges(e)));
+            }
+        }
+
+        queue = std::move(next);
+    }
+
+    co_return res;
+} catch (...) {
+    co_return result::current_exception();
 }
 
 }
