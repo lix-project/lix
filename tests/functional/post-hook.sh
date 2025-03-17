NixOS/nix

commit 450e5ec6185e2e1102e67ec7a348a0dc8955692d

Do not gate or hide experimental settings
This is somewhat hacky fix just for 2.15. I unintentionally hid them
from the manual, when no one wanted to hide them that (including
myself). I also required the experimental feature to be enabled in an
order-dependent way, which is not good.

The simplest fix for this immanent release is just to always show them,
and always allow them to be set.

Effectively undoes some changes from aa663b7e89d3d02248d37ee9f68b52770b247018

src/libutil/config.cc
@@ -70,17 +70,10 @@ void AbstractConfig::reapplyUnknownSettings()
         set(s.first, s.second);
 }
 
-// Whether we should process the option. Excludes aliases, which are handled elsewhere, and disabled features.
-static bool applicable(const Config::SettingData & sd)
-{
-    return !sd.isAlias
-        && experimentalFeatureSettings.isEnabled(sd.setting->experimentalFeature);
-}
-
 void Config::getSettings(std::map<std::string, SettingInfo> & res, bool overriddenOnly)
 {
     for (auto & opt : _settings)
-        if (applicable(opt.second) && (!overriddenOnly || opt.second.setting->overridden))
+        if (!opt.second.isAlias && (!overriddenOnly || opt.second.setting->overridden))
             res.emplace(opt.first, SettingInfo{opt.second.setting->to_string(), opt.second.setting->description});
 }
 
@@ -154,7 +147,7 @@ nlohmann::json Config::toJSON()
 {
     auto res = nlohmann::json::object();
     for (auto & s : _settings)
-        if (applicable(s.second))
+        if (!s.second.isAlias)
             res.emplace(s.first, s.second.setting->toJSON());
     return res;
 }
@@ -163,7 +156,7 @@ std::string Config::toKeyValue()
 {
     auto res = std::string();
     for (auto & s : _settings)
-        if (applicable(s.second))
+        if (s.second.isAlias)
             res += fmt("%s = %s\n", s.first, s.second.setting->to_string());
     return res;
 }
@@ -171,9 +164,6 @@ std::string Config::toKeyValue()
 void Config::convertToArgs(Args & args, const std::string & category)
 {
     for (auto & s : _settings) {
-        /* We do include args for settings gated on disabled
-           experimental-features. The args themselves however will also be
-           gated on any experimental feature the underlying setting is. */
         if (!s.second.isAlias)
             s.second.setting->convertToArg(args, category);
     }

tests/experimental-features.sh
@@ -1,25 +1,27 @@
 source common.sh
 
-# Without flakes, flake options should not show up
-# With flakes, flake options should show up
-
-function both_ways {
-    nix --experimental-features 'nix-command' "$@" | grepQuietInverse flake
-    nix --experimental-features 'nix-command flakes' "$@" | grepQuiet flake
-
-    # Also, the order should not matter
-    nix "$@" --experimental-features 'nix-command' | grepQuietInverse flake
-    nix "$@" --experimental-features 'nix-command flakes' | grepQuiet flake
-}
-
-# Simple case, the configuration effects the running command
-both_ways show-config
-
-# Skipping for now, because we actually *do* want these to show up in
-# the manual, just be marked experimental. Will reenable once the manual
-# generation takes advantage of the JSON metadata on this.
-
-# both_ways store gc --help
+# Skipping these two for now, because we actually *do* want flags and
+# config settings to always show up in the manual, just be marked
+# experimental. Will reenable once the manual generation takes advantage
+# of the JSON metadata on this.
+#
+# # Without flakes, flake options should not show up
+# # With flakes, flake options should show up
+#
+# function grep_both_ways {
+#     nix --experimental-features 'nix-command' "$@" | grepQuietInverse flake
+#     nix --experimental-features 'nix-command flakes' "$@" | grepQuiet flake
+#
+#     # Also, the order should not matter
+#     nix "$@" --experimental-features 'nix-command' | grepQuietInverse flake
+#     nix "$@" --experimental-features 'nix-command flakes' | grepQuiet flake
+# }
+#
+# # Simple case, the configuration effects the running command
+# grep_both_ways show-config
+#
+# # Medium case, the configuration effects --help
+# grep_both_ways store gc --help
 
 expect 1 nix --experimental-features 'nix-command' show-config --flake-registry 'https://no'
 nix --experimental-features 'nix-command flakes' show-config --flake-registry 'https://no'

lix-project/lix

commit 523965697d9a16f0dd89f0d5b795b44ce5d6f712

post-build-hook: don't emit config settings that aren't overridden
This is a redesign from 9b1f3cbc133eafdaadf89ee9e4f3ce6a11cbbcce where
this was introduced.

I deleted the AbstractConfig::toKeyValue since it was conspicuously and
obviously broken for two years since 450e5ec6185e2e1102e67ec7a348a0dc8955692d.
I asked myself if anyone was using it, given that it only emitted
settings that were aliases (broken!), and found that nobody used it.

The motivation for this change is the same for only emitting overridden
settings to the protocol: the nix inside there may not be able to parse
our defaults, as is the case of CppNix since the consensual
accept-flake-config was added to Lix.

Fixes: https://git.lix.systems/lix-project/lix/issues/739
Change-Id: Ib9874a52137f1f22220c25bcfa2425a4802509c7

lix/libstore/build/derivation-goal.cc
@@ -977,7 +977,7 @@ void runPostBuildHook(
     auto drvPathPretty = store.printStorePath(drvPath);
     hookEnvironment.emplace("DRV_PATH", drvPathPretty);
     hookEnvironment.emplace("OUT_PATHS", chomp(concatStringsSep(" ", store.printStorePathSet(outputPaths))));
-    hookEnvironment.emplace("NIX_CONFIG", globalConfig.toKeyValue());
+    hookEnvironment.emplace("NIX_CONFIG", globalConfig.toKeyValue(true));
 
     struct LogSink : Sink {
         Activity & act;

lix/libutil/config.cc
@@ -187,15 +187,6 @@ nlohmann::json Config::toJSON()
     return res;
 }
 
-std::string Config::toKeyValue()
-{
-    std::string res;
-    for (const auto & s : _settings)
-        if (s.second.isAlias)
-            res += fmt("%s = %s\n", s.first, s.second.setting->to_string());
-    return res;
-}
-
 void Config::convertToArgs(Args & args, const std::string & category)
 {
     for (auto & s : _settings) {
@@ -534,11 +525,11 @@ nlohmann::json GlobalConfig::toJSON()
     return res;
 }
 
-std::string GlobalConfig::toKeyValue()
+std::string GlobalConfig::toKeyValue(bool overriddenOnly)
 {
     std::string res;
     std::map<std::string, Config::SettingInfo> settings;
-    globalConfig.getSettings(settings);
+    globalConfig.getSettings(settings, overriddenOnly);
     for (const auto & s : settings)
         res += fmt("%s = %s\n", s.first, s.second.value);
     return res;

lix/libutil/config.hh
@@ -96,12 +96,6 @@ public:
     virtual nlohmann::json toJSON() = 0;
 
     /**
-     * Outputs all settings in a key-value pair format suitable to be used as
-     * `nix.conf`
-     */
-    virtual std::string toKeyValue() = 0;
-
-    /**
      * Converts settings to `Args` to be used on the command line interface
      * - args: args to write to
      * - category: category of the settings
@@ -165,8 +159,6 @@ public:
     void resetOverridden() override;
 
     nlohmann::json toJSON() override;
-
-    std::string toKeyValue() override;
 
     void convertToArgs(Args & args, const std::string & category) override;
 };
@@ -371,7 +363,11 @@ struct GlobalConfig : public AbstractConfig
 
     nlohmann::json toJSON() override;
 
-    std::string toKeyValue() override;
+    /**
+     * Outputs all settings in a key-value pair format suitable to be used as
+     * `nix.conf`
+     */
+    std::string toKeyValue(bool overriddenOnly = false);
 
     void convertToArgs(Args & args, const std::string & category) override;
 

tests/functional/post-hook.sh
@@ -29,3 +29,30 @@ clearStore
 nix copy --from "$REMOTE_STORE" --no-require-sigs -f dependencies.nix
 nix copy --from "$REMOTE_STORE" --no-require-sigs -f dependencies.nix input1_drv
 nix copy --from "$REMOTE_STORE" --no-require-sigs -f multiple-outputs.nix a^second
+
+clearStore
+
+# Should fail if the build hook fails
+cat > "$TEST_ROOT/fail.sh" <<-EOF
+#!${shell}
+false
+EOF
+chmod +x "$TEST_ROOT/fail.sh"
+expect 1 nix-build -o "$TEST_ROOT/result" dependencies.nix --post-build-hook "$TEST_ROOT/fail.sh"
+clearStore
+
+# Ensure that settings are passed into the post-build-hook, but only overridden
+# ones.
+rm -f "${TEST_ROOT}/nix-config"
+cat > "${TEST_ROOT}/settings.sh" <<-EOF
+#!${shell}
+echo "\$NIX_CONFIG" > "${TEST_ROOT}/nix-config"
+EOF
+chmod +x "${TEST_ROOT}/settings.sh"
+nix-build -o "${TEST_ROOT}/result" dependencies.nix --timeout 1337 --post-build-hook "${TEST_ROOT}/settings.sh"
+# Ensure pure-eval cannot become not a setting with the test passing.
+nix config show pure-eval
+# Defaulted setting does not appear.
+expect 1 grepQuiet pure-eval "${TEST_ROOT}/nix-config"
+# Overridden setting appears.
+grepQuiet "timeout = 1337" "${TEST_ROOT}/nix-config"