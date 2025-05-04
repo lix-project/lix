b17502088dee37c89c8969793bc7ff63336fc0e9
Commentator2.0 <commentator2.0@crystal-cavern.systems>
2025-05-04 19:30:25 +0200

    functional2: Added ruff formatter
    Ruff is used to enforce our code-style for the python parts of the
    reposity, similar to clang-tidy for the cpp parts.
    
    This includes a pre-commit hook to format code before it is committed
    When "unfixable" - i.e. no autoformatting is available - the commit is
    rejected
    
    resolves #812
    
    Change-Id: I6830c2fc29ae86337ec18f2b0e3565fac66c5523

--- misc/pre-commit.nix
+++ misc/pre-commit.nix
@@ -111,7 +111,10 @@ pre-commit-run {
     };
     treefmt = {
       enable = true;
-      settings.formatters = [ pkgs.nixfmt-rfc-style ];
+      settings.formatters = [
+        pkgs.nixfmt-rfc-style
+        pkgs.ruff
+      ];
     };
   };
 }
