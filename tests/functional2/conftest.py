573788f75a267129bb838b636f22805be5e84dc3
Commentator2.0 <commentator2.0@crystal-cavern.systems>
2025-05-12 16:18:32 +0200

    tests/functional2: add snapshot fixture
    Add a snapshot fixture, which allows comparing and updating strings
    against external files
    
    resolves #595
    
    Change-Id: I518f594c601eb7805c6492c0352fca753fda04c9

--- tests/functional2/conftest.py
+++ tests/functional2/conftest.py
@@ -3,4 +3,5 @@ pytest_plugins = (
     "functional2.testlib.fixtures.formatter",
     "functional2.testlib.fixtures.logger",
     "functional2.testlib.fixtures.nix",
+    "functional2.testlib.fixtures.snapshot",
 )
