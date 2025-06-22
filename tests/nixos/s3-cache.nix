lix-project/lix

commit 7453e2979f1d4684bea4cb340f23c089ea0d5a90

libstore: asyncify S3BinaryCacheStore
this has side-effects for FileTransfer as well since that uses S3Helper
for s3:// urls. the side effects should be entirely positive though: we
can run multiple s3 requests in parallel without explicitly running any
of them from thread pools (the aws s3 client takes care of that for us)

Change-Id: I67232e604ebb12982b63770f1661ea1d56c5087b

lix/libstore/s3-binary-cache-store.cc
@@ -5,16 +5,21 @@
 #include "lix/libstore/nar-info.hh"
 #include "lix/libstore/nar-info-disk-cache.hh"
 #include "lix/libstore/globals.hh"
+#include "lix/libutil/async.hh"
 #include "lix/libutil/async-io.hh"
 #include "lix/libutil/compression.hh"
 #include "lix/libstore/filetransfer.hh"
 #include "lix/libutil/result.hh"
 #include "lix/libutil/strings.hh"
 
+#include <kj/async.h>
+#include <memory>
+
 #include <aws/core/Aws.h>
 #include <aws/core/VersionConfig.h>
 #include <aws/core/auth/AWSCredentialsProvider.h>
 #include <aws/core/auth/AWSCredentialsProviderChain.h>
+#include <aws/core/client/AsyncCallerContext.h>
 #include <aws/core/client/ClientConfiguration.h>
 #include <aws/core/client/DefaultRetryStrategy.h>
 #include <aws/core/utils/logging/FormattedLogSystem.h>
@@ -25,6 +30,7 @@
 #include <aws/s3/model/HeadObjectRequest.h>
 #include <aws/s3/model/ListObjectsRequest.h>
 #include <aws/s3/model/PutObjectRequest.h>
+#include <aws/transfer/TransferHandle.h>
 #include <aws/transfer/TransferManager.h>
 
 using namespace Aws::Transfer;
@@ -148,9 +154,9 @@ ref<Aws::Client::ClientConfiguration> S3Helper::makeConfig(
     return res;
 }
 
-S3Helper::FileTransferResult S3Helper::getObject(
-    const std::string & bucketName, const std::string & key)
-{
+kj::Promise<Result<S3Helper::FileTransferResult>>
+S3Helper::getObject(const std::string & bucketName, const std::string & key)
+try {
     debug("fetching 's3://%s/%s'...", bucketName, key);
 
     auto request =
@@ -168,8 +174,17 @@ S3Helper::FileTransferResult S3Helper::getObject(
 
     try {
 
-        auto result = checkAws(fmt("AWS error fetching '%s'", key),
-            client->GetObject(request));
+        auto pfp = kj::newPromiseAndCrossThreadFulfiller<Aws::S3::Model::GetObjectOutcome>();
+        client->GetObjectAsync(
+            request,
+            [&](const Aws::S3::S3Client *,
+                const Aws::S3::Model::GetObjectRequest &,
+                Aws::S3::Model::GetObjectOutcome res,
+                const std::shared_ptr<const Aws::Client::AsyncCallerContext> &) {
+                pfp.fulfiller->fulfill(std::move(res));
+            }
+        );
+        auto result = checkAws(fmt("AWS error fetching '%s'", key), co_await pfp.promise);
 
         res.data = decompress(result.GetContentEncoding(),
             dynamic_cast<std::stringstream &>(result.GetBody()).str());
@@ -183,7 +198,9 @@ S3Helper::FileTransferResult S3Helper::getObject(
 
     res.durationMs = std::chrono::duration_cast<std::chrono::milliseconds>(now2 - now1).count();
 
-    return res;
+    co_return res;
+} catch (...) {
+    co_return result::current_exception();
 }
 
 struct S3BinaryCacheStoreConfig final : BinaryCacheStoreConfig
@@ -328,10 +345,17 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheStore
     try {
         stats.head++;
 
-        auto res = s3Helper.client->HeadObject(
-            Aws::S3::Model::HeadObjectRequest()
-            .WithBucket(bucketName)
-            .WithKey(path));
+        auto pfp = kj::newPromiseAndCrossThreadFulfiller<Aws::S3::Model::HeadObjectOutcome>();
+        s3Helper.client->HeadObjectAsync(
+            Aws::S3::Model::HeadObjectRequest().WithBucket(bucketName).WithKey(path),
+            [&](const Aws::S3::S3Client *,
+                const Aws::S3::Model::HeadObjectRequest &,
+                const Aws::S3::Model::HeadObjectOutcome & res,
+                const std::shared_ptr<const Aws::Client::AsyncCallerContext> &) {
+                pfp.fulfiller->fulfill(auto(res));
+            }
+        );
+        auto res = co_await pfp.promise;
 
         if (!res.IsSuccess()) {
             auto & error = res.GetError();
@@ -339,23 +363,25 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheStore
                 || error.GetErrorType() == Aws::S3::S3Errors::NO_SUCH_KEY
                 // If bucket listing is disabled, 404s turn into 403s
                 || error.GetErrorType() == Aws::S3::S3Errors::ACCESS_DENIED)
-                return {false};
+                co_return false;
             throw Error("AWS error fetching '%s': %s", path, error.GetMessage());
         }
 
-        return {true};
+        co_return true;
     } catch (...) {
-        return {result::current_exception()};
+        co_return result::current_exception();
     }
 
     std::shared_ptr<TransferManager> transferManager;
     std::once_flag transferManagerCreated;
 
-    void uploadFile(const std::string & path,
+    kj::Promise<Result<void>> uploadFile(
+        const std::string & path,
         std::shared_ptr<std::basic_iostream<char>> istream,
         const std::string & mimeType,
-        const std::string & contentEncoding)
-    {
+        const std::string & contentEncoding
+    )
+    try {
         istream->seekg(0, istream->end);
         auto size = istream->tellg();
         istream->seekg(0, istream->beg);
@@ -364,6 +390,15 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheStore
 
         static std::shared_ptr<Aws::Utils::Threading::PooledThreadExecutor>
             executor = std::make_shared<Aws::Utils::Threading::PooledThreadExecutor>(maxThreads);
+
+        struct TransferContext : Aws::Client::AsyncCallerContext
+        {
+            kj::CrossThreadPromiseFulfiller<void> * signal;
+            explicit TransferContext(kj::CrossThreadPromiseFulfiller<void> * signal)
+                : signal(signal)
+            {
+            }
+        };
 
         std::call_once(transferManagerCreated, [&]()
         {
@@ -384,6 +419,17 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheStore
                             transferHandle->GetKey(),
                             transferHandle->GetBytesTransferred(),
                             transferHandle->GetBytesTotalSize());
+
+                        if (transferHandle->GetStatus() != TransferStatus::NOT_STARTED
+                            && transferHandle->GetStatus() != TransferStatus::IN_PROGRESS)
+                        {
+                            auto context = std::static_pointer_cast<const TransferContext>(
+                                transferHandle->GetContext()
+                            );
+                            if (context->signal) {
+                                context->signal->fulfill();
+                            }
+                        }
                     };
 
                 transferManager = TransferManager::Create(transferConfig);
@@ -397,12 +443,17 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheStore
             if (contentEncoding != "")
                 throw Error("setting a content encoding is not supported with S3 multi-part uploads");
 
-            std::shared_ptr<TransferHandle> transferHandle =
-                transferManager->UploadFile(
-                    istream, bucketName, path, mimeType,
-                    Aws::Map<Aws::String, Aws::String>(),
-                    nullptr /*, contentEncoding */);
+            auto pfp = kj::newPromiseAndCrossThreadFulfiller<void>();
+            std::shared_ptr<TransferHandle> transferHandle = transferManager->UploadFile(
+                istream,
+                bucketName,
+                path,
+                mimeType,
+                Aws::Map<Aws::String, Aws::String>(),
+                std::make_shared<TransferContext>(pfp.fulfiller.get()) /*, contentEncoding */
+            );
 
+            co_await pfp.promise;
             transferHandle->WaitUntilFinished();
 
             if (transferHandle->GetStatus() == TransferStatus::FAILED)
@@ -427,8 +478,17 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheStore
 
             request.SetBody(istream);
 
-            auto result = checkAws(fmt("AWS error uploading '%s'", path),
-                s3Helper.client->PutObject(request));
+            auto pfp = kj::newPromiseAndCrossThreadFulfiller<Aws::S3::Model::HeadObjectOutcome>();
+            s3Helper.client->PutObjectAsync(
+                request,
+                [&](const Aws::S3::S3Client *,
+                    const Aws::S3::Model::PutObjectRequest &,
+                    const Aws::S3::Model::PutObjectOutcome & res,
+                    const std::shared_ptr<const Aws::Client::AsyncCallerContext> &) {
+                    pfp.fulfiller->fulfill(auto(res));
+                }
+            );
+            auto result = checkAws(fmt("AWS error uploading '%s'", path), co_await pfp.promise);
         }
 
         auto now2 = std::chrono::steady_clock::now();
@@ -443,6 +503,9 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheStore
         stats.putTimeMs += duration;
         stats.putBytes += std::max(size, (decltype(size)) 0);
         stats.put++;
+        co_return result::success();
+    } catch (...) {
+        co_return result::current_exception();
     }
 
     kj::Promise<Result<void>> upsertFile(
@@ -458,18 +521,22 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheStore
         };
 
         if (config().narinfoCompression != "" && path.ends_with(".narinfo")) {
-            uploadFile(
+            TRY_AWAIT(uploadFile(
                 path, compress(config().narinfoCompression), mimeType, config().narinfoCompression
-            );
+            ));
         } else if (config().lsCompression != "" && path.ends_with(".ls")) {
-            uploadFile(path, compress(config().lsCompression), mimeType, config().lsCompression);
+            TRY_AWAIT(
+                uploadFile(path, compress(config().lsCompression), mimeType, config().lsCompression)
+            );
         } else if (config().logCompression != "" && path.starts_with("log/"))
-            uploadFile(path, compress(config().logCompression), mimeType, config().logCompression);
+            TRY_AWAIT(uploadFile(
+                path, compress(config().logCompression), mimeType, config().logCompression
+            ));
         else
-            uploadFile(path, istream, mimeType, "");
-        return {result::success()};
+            TRY_AWAIT(uploadFile(path, istream, mimeType, ""));
+        co_return result::success();
     } catch (...) {
-        return {result::current_exception()};
+        co_return result::current_exception();
     }
 
     kj::Promise<Result<box_ptr<AsyncInputStream>>> getFile(const std::string & path) override
@@ -477,7 +544,7 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheStore
         stats.get++;
 
         // FIXME: stream output to sink.
-        auto res = s3Helper.getObject(bucketName, path);
+        auto res = TRY_AWAIT(s3Helper.getObject(bucketName, path));
 
         stats.getBytes += res.data ? res.data->size() : 0;
         stats.getTimeMs += res.durationMs;
@@ -486,15 +553,15 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheStore
             printTalkative("downloaded 's3://%s/%s' (%d bytes) in %d ms",
                 bucketName, path, res.data->size(), res.durationMs);
 
-            return {
-                make_box_ptr<AsyncGeneratorInputStream>([](std::string data) -> Generator<Bytes> {
+            co_return make_box_ptr<AsyncGeneratorInputStream>(
+                [](std::string data) -> Generator<Bytes> {
                     co_yield std::span{data.data(), data.size()};
-                }(std::move(*res.data)))
-            };
+                }(std::move(*res.data))
+            );
         } else
             throw NoSuchBinaryCacheFile("file '%s' does not exist in binary cache '%s'", path, getUri());
     } catch (...) {
-        return {result::current_exception()};
+        co_return result::current_exception();
     }
 
     kj::Promise<Result<StorePathSet>> queryAllValidPaths() override
@@ -505,12 +572,21 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheStore
         do {
             debug("listing bucket 's3://%s' from key '%s'...", bucketName, marker);
 
-            auto res = checkAws(fmt("AWS error listing bucket '%s'", bucketName),
-                s3Helper.client->ListObjects(
-                    Aws::S3::Model::ListObjectsRequest()
+            auto pfp = kj::newPromiseAndCrossThreadFulfiller<Aws::S3::Model::ListObjectsOutcome>();
+            s3Helper.client->ListObjectsAsync(
+                Aws::S3::Model::ListObjectsRequest()
                     .WithBucket(bucketName)
                     .WithDelimiter("/")
-                    .WithMarker(marker)));
+                    .WithMarker(marker),
+                [&](const Aws::S3::S3Client *,
+                    const Aws::S3::Model::ListObjectsRequest &,
+                    const Aws::S3::Model::ListObjectsOutcome & res,
+                    const std::shared_ptr<const Aws::Client::AsyncCallerContext> &) {
+                    pfp.fulfiller->fulfill(auto(res));
+                }
+            );
+            auto res =
+                checkAws(fmt("AWS error listing bucket '%s'", bucketName), co_await pfp.promise);
 
             auto & contents = res.GetContents();
 

lix/libstore/filetransfer.cc
@@ -730,13 +730,14 @@ struct curlFileTransfer : public FileTransfer
         co_return result::current_exception();
     }
 
-    std::optional<std::pair<FileTransferResult, box_ptr<AsyncInputStream>>> tryEagerTransfers(
+    kj::Promise<Result<std::optional<std::pair<FileTransferResult, box_ptr<AsyncInputStream>>>>>
+    tryEagerTransfers(
         const std::string & uri,
         const Headers & headers,
         const std::optional<std::string> & data,
         bool noBody
     )
-    {
+    try {
         // curl transfers using file:// urls cannot be paused, and are a bit unruly
         // in other ways too. since their metadata is trivial and we already have a
         // backend for simple file system reads we can use that instead. we'll pass
@@ -777,7 +778,9 @@ struct curlFileTransfer : public FileTransfer
                     );
                 }
                 if (S_ISDIR(st.st_mode)) {
-                    return {{std::move(metadata), make_box_ptr<AsyncStringInputStream>("")}};
+                    co_return std::pair{
+                        std::move(metadata), make_box_ptr<AsyncStringInputStream>("")
+                    };
                 }
                 struct OwningFdStream : AsyncInputStream
                 {
@@ -795,7 +798,9 @@ struct curlFileTransfer : public FileTransfer
                         }
                     }
                 };
-                return {{std::move(metadata), make_box_ptr<OwningFdStream>(std::move(fd))}};
+                co_return std::pair{
+                    std::move(metadata), make_box_ptr<OwningFdStream>(std::move(fd))
+                };
             }
         }
 
@@ -813,7 +818,7 @@ struct curlFileTransfer : public FileTransfer
             S3Helper s3Helper(profile, region, scheme, endpoint);
 
             // FIXME: implement ETag
-            auto s3Res = s3Helper.getObject(bucketName, key);
+            auto s3Res = TRY_AWAIT(s3Helper.getObject(bucketName, key));
             FileTransferResult res;
             if (!s3Res.data)
                 throw FileTransferError(NotFound, "S3 object '%s' does not exist", uri);
@@ -825,7 +830,7 @@ struct curlFileTransfer : public FileTransfer
                 {
                 }
             };
-            return {{ res, make_box_ptr<OwningStringStream>(std::move(*s3Res.data)) }};
+            co_return std::pair{res, make_box_ptr<OwningStringStream>(std::move(*s3Res.data))};
 #else
             throw nix::Error(
                 "cannot download '%s' because Lix is not built with S3 support", uri
@@ -833,7 +838,9 @@ struct curlFileTransfer : public FileTransfer
 #endif
         }
 
-        return std::nullopt;
+        co_return std::nullopt;
+    } catch (...) {
+        co_return result::current_exception();
     }
 
     kj::Promise<Result<std::pair<FileTransferResult, box_ptr<AsyncInputStream>>>>
@@ -844,7 +851,7 @@ struct curlFileTransfer : public FileTransfer
         bool noBody
     )
     try {
-        if (auto eager = tryEagerTransfers(uri, headers, data, noBody)) {
+        if (auto eager = TRY_AWAIT(tryEagerTransfers(uri, headers, data, noBody))) {
             co_return std::move(*eager);
         }
 

lix/libstore/s3.hh
@@ -3,8 +3,10 @@
 
 #if ENABLE_S3
 
+#include "lix/libutil/result.hh"
 #include "lix/libutil/ref.hh"
 
+#include <kj/async.h>
 #include <optional>
 #include <string>
 
@@ -28,8 +30,8 @@ struct S3Helper
         unsigned int durationMs;
     };
 
-    FileTransferResult getObject(
-        const std::string & bucketName, const std::string & key);
+    kj::Promise<Result<FileTransferResult>>
+    getObject(const std::string & bucketName, const std::string & key);
 };
 
 }

commit cd129186ea2bfbd13bf9170b820ca13711eb2310

libstore/s3: fulfill with `PutObjectOutcome` instead of `HeadObjectOutcome`
This was probably a typo introduced in
7453e2979f1d4684bea4cb340f23c089ea0d5a90.

Unfortunately, AWS SDK is so well made that this typo became an assert
error in production.

AWS Outcome constructors contains
```
            // Move error from other type of outcome
            template<typename RT, typename ET,
enable_if_t<!std::is_convertible<RT, R>::value &&

std::is_convertible<ET, E>::value, int> = 0>
```

which means that when:

* RT → R is not possible (e.g. PutObjectOutcome → HeadObjectOutcome)
* ET → E is possible (e.g. S3Error → S3Error)

Then, we will instantiate the error-moving outcome constructor which
asserts `!o.success`… Though, the original outcome indeed succeeded.

Change-Id: I3809514ae0648e8c02b0f93fa64d91115a091cd9
Co-authored-by: Maximilian Bosch <maximilian@mbosch.me>
Signed-off-by: Raito Bezarius <raito@lix.systems>

lix/libstore/s3-binary-cache-store.cc
@@ -478,7 +478,7 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheStore
 
             request.SetBody(istream);
 
-            auto pfp = kj::newPromiseAndCrossThreadFulfiller<Aws::S3::Model::HeadObjectOutcome>();
+            auto pfp = kj::newPromiseAndCrossThreadFulfiller<Aws::S3::Model::PutObjectOutcome>();
             s3Helper.client->PutObjectAsync(
                 request,
                 [&](const Aws::S3::S3Client *,

tests/nixos/default.nix
@@ -47,6 +47,8 @@ in
 
   remoteBuilds = runNixOSTestFor "x86_64-linux" ./remote-builds.nix;
 
+  s3-binary-cache = runNixOSTestFor "x86_64-linux" ./s3-cache.nix;
+
   # Test our Nix as a client against remotes that are older
 
   remoteBuilds_remote_2_3 = runNixOSTestFor "x86_64-linux" {

tests/nixos/s3-cache.nix
@@ -1,0 +1,155 @@
+{
+  lib,
+  config,
+  ...
+}:
+
+let
+  garage-ephemeral-key = pkgs.callPackage ../../releng/garage-ephemeral-key {
+    inherit (pkgs.writers) writePython3Bin;
+  };
+
+  pkgs = config.nodes.client.nixpkgs.pkgs;
+
+  pkgA = pkgs.cowsay;
+  pkgB = pkgs.hello;
+in {
+  name = "s3-cache";
+
+  nodes.client = { pkgs, lib, ... }: {
+    virtualisation.writableStore = true;
+    virtualisation.additionalPaths = [ pkgA pkgB ];
+
+    nix.settings.substituters = lib.mkForce [ ];
+    nix.settings.experimental-features = [ "nix-command" ];
+
+    environment.systemPackages = with pkgs; [
+      awscli2
+      brotli
+    ];
+  };
+
+  nodes.s3 = { pkgs, ... }: {
+    services.garage.enable = true;
+    services.garage.package = pkgs.garage_1_x;
+    services.garage.settings = {
+      replication_factor = 1;
+
+      rpc_bind_addr = "[::]:3901";
+      rpc_secret = "4425f5c26c5e11581d3223904324dcb5b5d5dfb14e5e7f35e38c595424f5f1e6";
+
+      s3_api.api_bind_addr = "[::]:3900";
+      s3_api.s3_region = "garage";
+      s3_api.root_domain = ".s3";
+
+      admin = {
+        api_bind_addr = "[::]:3903";
+        admin_token = "UkLeGWEvHnXBqnueR3ISEMWpOnm40jH2tM2HnnL/0F4=";
+      };
+    };
+
+    networking.firewall.allowedTCPPorts = [ 3900 ];
+
+    environment.sessionVariables = {
+      GARAGE_ADMIN_TOKEN = "UkLeGWEvHnXBqnueR3ISEMWpOnm40jH2tM2HnnL/0F4=";
+    };
+
+    environment.systemPackages = [
+      pkgs.garage_1_x
+      pkgs.git
+      pkgs.build-release-notes
+      pkgs.jq
+      pkgs.nix-eval-jobs
+      garage-ephemeral-key
+    ];
+  };
+
+  testScript = ''
+    import json
+    import textwrap
+
+    start_all()
+
+    client.wait_for_unit("multi-user.target")
+    s3.wait_for_unit("garage")
+    s3.wait_for_open_port(3900)
+
+    def run_test_packages(fail=False):
+        fun = client.succeed if not fail else client.fail
+        fun("${lib.getExe pkgA} <<<awoo >&2")
+        fun("${lib.getExe pkgB} >&2")
+
+    def setup_s3():
+        nodeId = s3.succeed("garage node id")
+        s3.succeed(f"garage layout assign -z dc1 -c 10G {nodeId}")
+        s3.succeed("garage layout apply --version 1")
+        s3.succeed("garage bucket create cache")
+
+        out = json.loads(
+            s3.succeed("garage-ephemeral-key new --name cache --read --write --age-sec 7200 cache")
+        )
+
+        aws_config = textwrap.dedent(f"""
+        [default]
+        endpoint_url = http://s3:3900
+        aws_access_key_id = {out['id']}
+        aws_secret_access_key = {out['secret_key']}
+        region = garage
+        """)
+
+        with open("aws-credentials", "w") as f:
+            f.write(aws_config)
+
+        client.copy_from_host("aws-credentials", "/root/.aws/credentials")
+
+    setup_s3()
+
+    with subtest("Ensure that communication with S3 works"):
+        t.assertEqual("", client.succeed("aws s3 ls s3://cache/").rstrip())
+
+    STORE_URI = "s3://cache?write-nar-listing=1&ls-compression=br&compression=zstd&parallel-compression=true&region=garage&endpoint=s3:3900&scheme=http"
+
+    with subtest("Copy packages to S3"):
+        client.succeed(f"nix copy --to '{STORE_URI}' ${pkgA} ${pkgB}")
+
+        # Cache isn't empty anymore.
+        t.assertNotEqual("", client.succeed("aws s3 ls s3://cache/"))
+
+    with subtest("Ensure narinfo, listing and nar exist"):
+        for store_path in ["${baseNameOf pkgA}", "${baseNameOf pkgB}"]:
+            hash_part = store_path.split("-")[0]
+
+            client.succeed(f"aws s3 cp s3://cache/{hash_part}.narinfo .")
+
+            narinfo = {}
+            for line in client.succeed(f"cat {hash_part}.narinfo").strip().splitlines():
+                key, value = line.strip().split(": ", 1)
+                narinfo[key] = value
+
+            t.assertEqual("zstd", narinfo["Compression"])
+            t.assertEqual(f"/nix/store/{store_path}", narinfo["StorePath"])
+
+            url = narinfo["URL"]
+            client.succeed(f"aws s3 ls s3://cache/{url} >&2")
+
+            client.succeed(f"aws s3 cp s3://cache/{hash_part}.ls .")
+            listing = json.loads(client.succeed(f"cat {hash_part}.ls | brotli -d"))
+
+            t.assertIn("root", listing)
+            root = listing["root"]["entries"]
+            t.assertIn("bin", root)
+            t.assertEqual("directory", root["bin"]["type"])
+
+    with subtest("Ensure that the path is substitutable"):
+        run_test_packages()
+
+        client.succeed("nix-store --delete ${pkgA}")
+        client.succeed("nix-store --delete ${pkgB}")
+
+        run_test_packages(fail=True)
+
+        client.succeed(f"nix copy --from '{STORE_URI}' ${pkgA} ${pkgB} --no-check-sigs")
+
+        run_test_packages()
+  '';
+}