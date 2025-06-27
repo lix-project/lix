d1db3e5fa3faa43b3d2f2e2e843e9cfc1e6e1b71
Emily <hello@emily.moe>
2025-06-27 14:42:07 +0100

    libstore: fix Unix sockets in the build directory on sandboxed macOS
    We’re already allowing `/tmp` anyway, so this should be harmless,
    and it fixes a regression in the default configuration caused by
    moving the build directories out of `temp-dir`. (For instance, that
    broke the Lix `guessOrInventPath.sockets` test.)
    
    Note that removing `/tmp` breaks quite a few builds, so although it may
    be a good idea in general it would require work on the Nixpkgs side.
    
    Fixes: 749afbbe99fd7b45f828b72628252feba9241362
    Change-Id: I6a6a69645f429bc50d4cb24283feda3d3091f534

--- lix/libstore/build/local-derivation-goal.cc
+++ lix/libstore/build/local-derivation-goal.cc
@@ -1471,6 +1471,8 @@ void LocalDerivationGoal::runChild()
 
         if (getEnv("_NIX_TEST_NO_SANDBOX") != "1") {
             Strings sandboxArgs;
+            sandboxArgs.push_back("_NIX_BUILD_TOP");
+            sandboxArgs.push_back(tmpDir);
             sandboxArgs.push_back("_GLOBAL_TMP_DIR");
             sandboxArgs.push_back(globalTmpDir);
             if (allowLocalNetworking) {
