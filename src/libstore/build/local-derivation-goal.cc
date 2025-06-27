4ef56601b5a235949dc14b8852aa05580ffaefd9
eldritch horrors <pennae@lix.systems>
2025-06-27 15:38:53 +0200

    libstore: don't delete already valid outputs after build
    eagerly consider outputs as not needing deletion during output
    registration rather than only doing so after registration. not
    waiting for registration to succeed may keep store paths alive
    in the file system if registration fails for some reason; that
    seem preferrable to the possibility of having another instance
    of this bug. since we only leave *good* outputs around there's
    not much to worry about except maybe bit of wasted disk space.
    
    fixes #883
    
    Change-Id: I8c22c92e39b9e203f1061278f86cde19dc4474a4

--- src/libstore/build/local-derivation-goal.cc
+++ src/libstore/build/local-derivation-goal.cc
@@ -2098,6 +2098,11 @@ SingleDrvOutputs LocalDerivationGoal::registerOutputs()
                before this for loop. */
             if (*scratchPath != finalStorePath)
                 outputRewrites[std::string { scratchPath->hashPart() }] = std::string { finalStorePath.hashPart() };
+            /* Cancel automatic deletion of that output if it was a scratch output that we just
+             * registered. */
+            if (auto cleaner = scratchOutputsCleaner.extract(outputName)) {
+                cleaner.mapped().cancel();
+            }
         };
 
         auto orifu = get(outputReferencesIfUnregistered, outputName);
@@ -2413,10 +2418,6 @@ SingleDrvOutputs LocalDerivationGoal::registerOutputs()
            the next iteration */
         if (newInfo.ca) {
             localStore.registerValidPaths({{newInfo.path, newInfo}});
-            /* Cancel automatic deletion of that output if it was a scratch output. */
-            if (auto cleaner = scratchOutputsCleaner.extract(outputName)) {
-                cleaner.mapped().cancel();
-            }
         }
 
         infos.emplace(outputName, std::move(newInfo));
@@ -2445,13 +2446,6 @@ SingleDrvOutputs LocalDerivationGoal::registerOutputs()
             infos2.insert_or_assign(newInfo.path, newInfo);
         }
         localStore.registerValidPaths(infos2);
-
-        /* Cancel automatic deletion of that output if it was a scratch output that we just registered. */
-        for (auto & [outputName, _ ] : infos) {
-            if (auto cleaner = scratchOutputsCleaner.extract(outputName)) {
-                cleaner.mapped().cancel();
-            }
-        }
     }
 
     /* In case of a fixed-output derivation hash mismatch, throw an
