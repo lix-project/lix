lix-project/lix

commit 9a59106c172b7d5963e3dc2cf07ff5b19f8119d6

libstore: `NIX_REMOTE=auto` tries the daemon socket *then* direct access
In the past, it tried direct access if it *could* [1] perform direct
access.

This solves a bunch of errors people had when they tried the cgroup
feature and their scripts did not pass NIX_REMOTE=daemon manually
(nixos-rebuild-ng, home-manager activation from a root systemd unit,
etc.)

To avoid looping infinitely while receiving daemon connections, we
forcibly change the store URI when forking for a subdaemon to do direct
access automatically, this doesn't break forward usecases where you
point a daemon to another socket because we only change NIX_REMOTE="",
NIX_REMOTE=daemon, NIX_REMOTE=auto to a local and direct access.

All these usecases would end up infinitely looping no matter what
settings are set, because we are also responsible for creating the
daemon socket.

[1]: this happened all the time if you were `root`.

Related: https://github.com/NixOS/nixpkgs/pull/415701
Change-Id: I783fc795a9c2ee25b3d9f44f453f8f94b063371f
Signed-off-by: Raito Bezarius <raito@lix.systems>

lix/nix/daemon.cc
@@ -5,6 +5,7 @@
 #include "lix/libstore/local-store.hh"
 #include "lix/libstore/remote-store.hh"
 #include "lix/libstore/remote-store-connection.hh"
+#include "lix/libstore/store-api.hh"
 #include "lix/libutil/serialise.hh"
 #include "lix/libutil/archive.hh"
 #include "lix/libstore/globals.hh"
@@ -229,7 +230,7 @@ try {
     StoreConfig::Params params; // FIXME: get params from somewhere
     // Disable caching since the client already does that.
     params["path-info-cache-size"] = "0";
-    co_return TRY_AWAIT(openStore(settings.storeUri, params));
+    co_return TRY_AWAIT(openNonDaemonStore(settings.storeUri, params));
 } catch (...) {
     co_return result::current_exception();
 }

tests/nixos/cgroups/default.nix
@@ -24,7 +24,7 @@
     host.wait_for_unit("multi-user.target")
 
     # Start build in background
-    host.execute("NIX_REMOTE=daemon nix build --use-cgroups --auto-allocate-uids --file ${./hang.nix} >&2 &")
+    host.execute("nix build --use-cgroups --auto-allocate-uids --file ${./hang.nix} >&2 &")
     pid = int(host.succeed("pgrep nix"))
     service = "/sys/fs/cgroup/system.slice/nix-daemon.service"
 

lix/legacy/build-remote.cc
@@ -84,7 +84,13 @@ static int main_build_remote(AsyncIoRoot & aio, std::string programName, Strings
 
         initPlugins();
 
-        auto store = aio.blockOn(openStore());
+        // FIXME this does not open a daemon connection for historical reasons.
+        // we may create a lot of build hook instances, and having each of them
+        // also create a daemon instance is inefficient and wasteful. in future
+        // versions of the build hook (where we don't need one hook process per
+        // build) we should change this to using a daemon connection, ideally a
+        // daemon connection provided by the parent via file descriptor passing
+        auto store = aio.blockOn(openNonDaemonStore());
 
         /* It would be more appropriate to use $XDG_RUNTIME_DIR, since
            that gets cleared on reboot, but it wouldn't work on macOS. */

lix/libstore/store-api.cc
@@ -1367,20 +1367,19 @@ static bool isNonUriPath(const std::string & spec)
         && spec.find("/") != std::string::npos;
 }
 
-static std::optional<ref<Store>> openFromNonUri(const std::string & uri, const StoreConfig::Params & params)
+static std::optional<ref<Store>>
+openFromNonUri(const std::string & uri, const StoreConfig::Params & params, bool allowDaemon)
 {
     if (uri == "" || uri == "auto") {
         auto stateDir = getOr(params, "state", settings.nixStateDir);
-        if (access(stateDir.c_str(), R_OK | W_OK) == 0)
-            return LocalStore::makeLocalStore(params);
-        else if (pathExists(settings.nixDaemonSocketFile))
+        if (allowDaemon && pathExists(settings.nixDaemonSocketFile)) {
             return make_ref<UDSRemoteStore>(params);
-        #if __linux__
-        else if (!pathExists(stateDir)
-            && params.empty()
-            && getuid() != 0
-            && !getEnv("NIX_STORE_DIR").has_value()
-            && !getEnv("NIX_STATE_DIR").has_value())
+        } else if (access(stateDir.c_str(), R_OK | W_OK) == 0) {
+            return LocalStore::makeLocalStore(params);
+        }
+#if __linux__
+        else if (!pathExists(stateDir) && params.empty() && getuid() != 0
+                 && !getEnv("NIX_STORE_DIR").has_value() && !getEnv("NIX_STATE_DIR").has_value())
         {
             /* If /nix doesn't exist, there is no daemon socket, and
                we're not root, then automatically set up a chroot
@@ -1404,6 +1403,9 @@ static std::optional<ref<Store>> openFromNonUri(const std::string & uri, const S
         else
             return LocalStore::makeLocalStore(params);
     } else if (uri == "daemon") {
+        if (!allowDaemon) {
+            throw Error("tried to open a daemon store in a context that doesn't support this");
+        }
         return make_ref<UDSRemoteStore>(params);
     } else if (uri == "local") {
         return LocalStore::makeLocalStore(params);
@@ -1444,8 +1446,8 @@ static std::string extractConnStr(const std::string &proto, const std::string &c
     return connStr;
 }
 
-kj::Promise<Result<ref<Store>>> openStore(const std::string & uri_,
-    const StoreConfig::Params & extraParams)
+static kj::Promise<Result<ref<Store>>>
+openStore(const std::string & uri_, const StoreConfig::Params & extraParams, bool allowDaemon)
 try {
     auto params = extraParams;
     try {
@@ -1473,7 +1475,7 @@ try {
         auto [uri, uriParams] = splitUriAndParams(uri_);
         params.insert(uriParams.begin(), uriParams.end());
 
-        if (auto store = openFromNonUri(uri, params)) {
+        if (auto store = openFromNonUri(uri, params, allowDaemon)) {
             (*store)->config().warnUnknownSettings();
             co_return *store;
         }
@@ -1482,6 +1484,18 @@ try {
     throw Error("don't know how to open Nix store '%s'", uri_);
 } catch (...) {
     co_return result::current_exception();
+}
+
+kj::Promise<Result<ref<Store>>>
+openStore(const std::string & uri, const StoreConfig::Params & extraParams)
+{
+    return openStore(uri, extraParams, true);
+}
+
+kj::Promise<Result<ref<Store>>>
+openNonDaemonStore(const std::string & uri, const StoreConfig::Params & extraParams)
+{
+    return openStore(uri, extraParams, false);
 }
 
 kj::Promise<Result<std::list<ref<Store>>>> getDefaultSubstituters()

lix/libstore/store-api.hh
@@ -983,9 +983,7 @@ resolveDerivedPath(Store &, const DerivedPath::Built &, Store * evalStore = null
  * - ‘unix://<path>’: The Nix store accessed via a Unix domain socket
  *   connection to nix-daemon, with the socket located at <path>.
  *
- * - ‘auto’ or ‘’: Equivalent to ‘local’ or ‘daemon’ depending on
- *   whether the user has write access to the local Nix
- *   store/database.
+ * - ‘auto’ or ‘’: Try `daemon` if the daemon socket exists and `local` otherwise.
  *
  * - ‘file://<path>’: A binary cache stored in <path>.
  *
@@ -1003,6 +1001,14 @@ resolveDerivedPath(Store &, const DerivedPath::Built &, Store * evalStore = null
 kj::Promise<Result<ref<Store>>> openStore(const std::string & uri = settings.storeUri.get(),
     const StoreConfig::Params & extraParams = {});
 
+/**
+ * Same as `openStore`, but no connections to the daemon are attempted.
+ * `""` and `"auto"` urls will only attempt the `local` methods, `"daemon"`
+ * urls will cause a hard error.
+ */
+kj::Promise<Result<ref<Store>>> openNonDaemonStore(
+    const std::string & uri = settings.storeUri.get(), const StoreConfig::Params & extraParams = {}
+);
 
 /**
  * @return the default substituter stores, defined by the

tests/nixos/containers/containers.nix
@@ -50,25 +50,25 @@
     host.fail("nix build -v --auto-allocate-uids --no-use-cgroups --no-sandbox -L --offline --impure --file ${./id-test.nix} --argstr name id-test-4")
 
     # Auto-allocated UIDs, UID range, sandbox, via daemon.
-    host.succeed("NIX_REMOTE=daemon nix build -v --auto-allocate-uids --sandbox -L --offline --impure --file ${./id-test.nix} --argstr name id-test-5 --arg uidRange true")
+    host.succeed("nix build -v --auto-allocate-uids --sandbox -L --offline --impure --file ${./id-test.nix} --argstr name id-test-5 --arg uidRange true")
     host.succeed("[[ $(cat ./result) = 'uid=0(root) gid=0(root) groups=0(root)' ]]")
 
     # Auto-allocated UIDs, UID range, no sandbox, with and without daemon.
-    host.fail("NIX_REMOTE=daemon nix build -v --auto-allocate-uids --no-sandbox -L --offline --impure --file ${./id-test.nix} --argstr name id-test-6 --arg uidRange true")
+    host.fail("nix build -v --auto-allocate-uids --no-sandbox -L --offline --impure --file ${./id-test.nix} --argstr name id-test-6 --arg uidRange true")
     host.fail("nix build -v --auto-allocate-uids --no-use-cgroups --no-sandbox -L --offline --impure --file ${./id-test.nix} --argstr name id-test-7 --arg uidRange true")
 
     # Run systemd-nspawn in a Nix build, via daemon.
-    host.succeed("NIX_REMOTE=daemon nix build -vv --auto-allocate-uids --sandbox -L --offline --impure --file ${./systemd-nspawn.nix} --argstr nixpkgs ${nixpkgs}")
+    host.succeed("nix build -vv --auto-allocate-uids --sandbox -L --offline --impure --file ${./systemd-nspawn.nix} --argstr nixpkgs ${nixpkgs}")
     host.succeed("[[ $(cat ./result/msg) = 'Hello World' ]]")
 
     # Auto-allocated UIDs, UID range, sandbox, WITHOUT daemon (so-called: Nix as root), IN `systemd-run` transient scope.
     # `systemd-run` is CRITICAL to run this successfully.
-    host.succeed("systemd-run --same-dir --wait -p Delegate=yes -p DelegateSubgroup=supervisor nix build -v --auto-allocate-uids --sandbox -L --offline --impure --file ${./id-test.nix} --argstr name id-test-8 --arg uidRange true -I nixpkgs=${nixpkgs}")
+    host.succeed("systemd-run --same-dir --wait -E NIX_REMOTE=local -p Delegate=yes -p DelegateSubgroup=supervisor nix build -v --auto-allocate-uids --sandbox -L --offline --impure --file ${./id-test.nix} --argstr name id-test-8 --arg uidRange true -I nixpkgs=${nixpkgs}")
     host.succeed("[[ $(cat ./result) = 'uid=0(root) gid=0(root) groups=0(root)' ]]")
 
     # Run systemd-nspawn in a Nix build, WITHOUT daemon (so-called: Nix as root) IN `systemd-run`.
     # `systemd-run` is CRITICAL to run this successfully.
-    host.succeed("systemd-run --same-dir --wait -p Delegate=yes -p DelegateSubgroup=supervisor nix build -v --auto-allocate-uids --sandbox -L --offline --impure --file ${./systemd-nspawn.nix} --argstr nixpkgs ${nixpkgs} -I nixpkgs=${nixpkgs}")
+    host.succeed("systemd-run --same-dir --wait -E NIX_REMOTE=local -p Delegate=yes -p DelegateSubgroup=supervisor nix build -v --auto-allocate-uids --sandbox -L --offline --impure --file ${./systemd-nspawn.nix} --argstr nixpkgs ${nixpkgs} -I nixpkgs=${nixpkgs}")
     host.succeed("[[ $(cat ./result/msg) = 'Hello World' ]]")
   '';
 

tests/nixos/fetchurl.nix
@@ -79,6 +79,6 @@ in
     assert "SSL certificate problem: self-signed certificate" in err
 
     # Fetching from a server with a trusted cert should work via environment variable override.
-    machine.succeed("NIX_SSL_CERT_FILE=/tmp/cafile.pem nix build --no-substitute --expr 'import <nix/fetchurl.nix> { url = \"https://bad/index.html\"; hash = \"sha256-rsBwZF/lPuOzdjBZN2E08FjMM3JHyXit0Xi2zN+wAZ8=\"; }'")
+    machine.succeed("NIX_SSL_CERT_FILE=/tmp/cafile.pem NIX_REMOTE=local nix build --no-substitute --expr 'import <nix/fetchurl.nix> { url = \"https://bad/index.html\"; hash = \"sha256-rsBwZF/lPuOzdjBZN2E08FjMM3JHyXit0Xi2zN+wAZ8=\"; }'")
   '';
 }

commit e1ccbe9abd5042e5622379bcae35a81bfd242330

daemon: prefer daemon stores for `nix-daemon --stdio`
Using `AllowDaemon::Disallow` here broke `ssh-ng://` remote builds in
multi‐user setups where the remote builder user does not have write
access to the store, now that the automatic store selection logic
has changed. Switch to the default behaviour for this path to fix that.

This causes `ssh-ng://` builds to use the daemon by default on the
remote end, even as `root`. I think this is desirable, as the previous
change already made `ssh://` behave this way, and the pitfalls of
local stores apply to remote builds too. For instance, there were
persistent `ulimit` issues on the NixOS Hydra macOS builders that were
resolved by forcing use of the daemon, and I believe the Linux builders
also go through the daemon these days due to using non‐`root` SSH
users. I believe that the `root` vs. non‐`root` difference is just
as confusing for remote builds as it is for local ones.

`ssh-ng://root@builder?remote-store=local` can be used to revert back
to the previous default if necessary.

Closes: #884
Fixes: 9a59106c172b7d5963e3dc2cf07ff5b19f8119d6
Change-Id: I6a6a696410f46cd3f2f5a94073ea924ad45dc99c

lix/nix/daemon.cc
@@ -225,12 +225,12 @@ static PeerInfo getPeerInfo(int remote)
 /**
  * Open a store without a path info cache.
  */
-static kj::Promise<Result<ref<Store>>> openUncachedStore()
+static kj::Promise<Result<ref<Store>>> openUncachedStore(AllowDaemon allowDaemon = AllowDaemon::Allow)
 try {
     StoreConfig::Params params; // FIXME: get params from somewhere
     // Disable caching since the client already does that.
     params["path-info-cache-size"] = "0";
-    co_return TRY_AWAIT(openStore(settings.storeUri, params, AllowDaemon::Disallow));
+    co_return TRY_AWAIT(openStore(settings.storeUri, params, allowDaemon));
 } catch (...) {
     co_return result::current_exception();
 }
@@ -369,7 +369,7 @@ static void daemonLoopImpl(std::optional<TrustedFlag> forceTrustClientOpt)
                 FdSource from(remote.get());
                 FdSink to(remote.get());
                 processConnection(
-                    aio, aio.blockOn(openUncachedStore()), from, to, trusted
+                    aio, aio.blockOn(openUncachedStore(AllowDaemon::Disallow)), from, to, trusted
                 );
 
                 exit(0);

tests/nixos/default.nix
@@ -122,8 +122,6 @@ in
       };
     };
     sshUser = "test-user";
-    # FIXME: <https://git.lix.systems/lix-project/lix/issues/884>
-    expectSuccess = false;
   };
 
   # Test our Nix as a client against remotes that are older

tests/nixos/remote-builds-ssh-ng.nix
@@ -44,14 +44,6 @@ in
       '';
       default = "root";
     };
-
-    expectSuccess = lib.mkOption {
-      type = lib.types.bool;
-      description = ''
-        Whether to expect the remote build to succeed or not.
-      '';
-      default = true;
-    };
   };
 
   config = {
@@ -123,9 +115,8 @@ in
       client.succeed(f"ssh -o StrictHostKeyChecking=no {ssh_user}@{builder.name} 'echo hello world' >&2")
 
       # Perform a build
-      out = client.${if test.config.expectSuccess then "succeed" else "fail"}("nix-build ${expr nodes.client 1} 2> build-output")
+      out = client.succeed("nix-build ${expr nodes.client 1} 2> build-output")
 
-    '' + lib.optionalString test.config.expectSuccess ''
       # Verify that the build was done on the builder
       builder.succeed(f"test -e {out.strip()}")
 