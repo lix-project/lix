65ef28df6dac2b5fe3b405a534515567a2f60b6f
Matthew Bauer <mjbauer95@gmail.com>
2025-07-10 17:59:27 -0500

    completions: Show description in zsh completions
    Uses the second part of NIX_GET_COMPLETIONS if available.
    
    Change-Id: I35efaaea3f35eea25a8de723ab32ceecd054e7df
    Fixes: https://git.lix.systems/lix-project/lix/issues/910

--- misc/zsh/completion.zsh
+++ misc/zsh/completion.zsh
@@ -1,24 +1,28 @@
 #compdef nix
 
 function _nix() {
-  local ifs_bk="$IFS"
   local input=("${(Q)words[@]}")
-  IFS=$'\n'
-  local res=($(NIX_GET_COMPLETIONS=$((CURRENT - 1)) "$input[@]" 2>/dev/null))
+  local -a suggestions # what value to actually use on completion
+  local -a suggestions_display # what to display to the user
+  local tpe suggestion description
+  local ifs_bk="$IFS"
+  IFS=$'\t'
+  NIX_GET_COMPLETIONS=$((CURRENT - 1)) "$input[@]" 2>/dev/null | {read tpe; while IFS=$'\t' read -r suggestion description; do
+    suggestions+=("$suggestion")
+    if [ -n "$description" ]; then
+      suggestions_display+=("$suggestion -- $description")
+    else
+      suggestions_display+=("$suggestion")
+    fi
+  done}
   IFS="$ifs_bk"
-  local tpe="${${res[1]}%%>	*}"
-  local -a suggestions
-  declare -a suggestions
-  for suggestion in ${res:1}; do
-    suggestions+=("${suggestion%%	*}")
-  done
   local -a args
   if [[ "$tpe" == filenames ]]; then
     args+=('-f')
   elif [[ "$tpe" == attrs ]]; then
     args+=('-S' '')
   fi
-  compadd -J nix "${args[@]}" -a suggestions
+  compadd -J nix "${args[@]}" -d suggestions_display -a suggestions
 }
 
 _nix "$@"
