lix-project/lix

commit e6da29ad6b6d15b67fdd6f862f2a939a4542ee7b

packaging: use the upstream `mesonCheckPhase` for `installCheckPhase`
This gets us `--print-errorlogs` and `--timeout-multiplier=0` for free,
and also fixes the concatenation logic with `__structuredAttrs`.

Change-Id: I6a6a69643ea7224cb24508505c554143f83ae243

package.nix
@@ -450,12 +450,11 @@ stdenv.mkDerivation (finalAttrs: {
     "--print-errorlogs"
   ];
 
-  installCheckPhase = ''
-    runHook preInstallCheck
-    flagsArray=($mesonInstallCheckFlags "''${mesonInstallCheckFlagsArray[@]}")
-    meson test --no-rebuild "''${flagsArray[@]}"
-    runHook postInstallCheck
+  preInstallCheck = ''
+    mesonCheckFlags=("''${mesonInstallCheckFlags[@]}")
   '';
+
+  installCheckPhase = "mesonCheckPhase";
 
   separateDebugInfo = !hostPlatform.isStatic && !finalAttrs.dontBuild;
 

commit 52872026fb77054c51a9b79c6ae93a17ecec8d89

packaging: actually run install checks
`mesonCheckPhase` of course runs `preCheck` and `postCheck` hooks,
not `preInstallCheck`, so this was not doing the right thing. Really
sorry for breaking CI :(

Fixes: e6da29ad6b6d15b67fdd6f862f2a939a4542ee7b
Change-Id: I6a6a69642a242e2c8bfe10ac61d3f6756d666548

package.nix
@@ -444,11 +444,17 @@ stdenv.mkDerivation (finalAttrs: {
     "--suite=installcheck"
   ];
 
-  preInstallCheck = ''
-    mesonCheckFlags=("''${mesonInstallCheckFlags[@]}")
-  '';
+  installCheckPhase = ''
+    runHook preInstallCheck
 
-  installCheckPhase = "mesonCheckPhase";
+    (
+      unset -v preCheck preCheckHooks postCheck postCheckHooks
+      mesonCheckFlags=("''${mesonInstallCheckFlags[@]}")
+      mesonCheckPhase
+    )
+
+    runHook postInstallCheck
+  '';
 
   separateDebugInfo = !hostPlatform.isStatic && !finalAttrs.dontBuild;
 