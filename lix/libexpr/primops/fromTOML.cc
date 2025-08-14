96a39dc464165a3e503a6dc7bd44518a116fe846
Emily <hello@emily.moe>
2025-08-14 16:46:46 +0100

    libexpr: format `fromTOML` source
    Otherwise the next diff becomes very messy.
    
    Change-Id: I6a6a6964d96543ade130d491f413ebd9fe2b7ff1
    (cherry picked from commit c586596a9fba2b95a5bba1d72b8039d7c7e8d75e)

--- lix/libexpr/primops/fromTOML.cc
+++ lix/libexpr/primops/fromTOML.cc
@@ -6,77 +6,83 @@
 
 namespace nix {
 
-void prim_fromTOML(EvalState & state, Value * * args, Value & val)
+void prim_fromTOML(EvalState & state, Value ** args, Value & val)
 {
-    auto toml = state.forceStringNoCtx(*args[0], noPos, "while evaluating the argument passed to builtins.fromTOML");
+    auto toml = state.forceStringNoCtx(
+        *args[0], noPos, "while evaluating the argument passed to builtins.fromTOML"
+    );
 
     std::istringstream tomlStream(std::string{toml});
 
     std::function<void(Value &, toml::value)> visit;
 
     visit = [&](Value & v, toml::value t) {
+        switch (t.type()) {
+        case toml::value_t::table: {
+            auto table = toml::get<toml::table>(t);
 
-        switch(t.type())
-        {
-            case toml::value_t::table:
-                {
-                    auto table = toml::get<toml::table>(t);
+            size_t size = 0;
+            for (auto & i : table) {
+                (void) i;
+                size++;
+            }
 
-                    size_t size = 0;
-                    for (auto & i : table) { (void) i; size++; }
+            auto attrs = state.ctx.buildBindings(size);
 
-                    auto attrs = state.ctx.buildBindings(size);
+            for (auto & elem : table) {
+                visit(attrs.alloc(elem.first), elem.second);
+            }
 
-                    for(auto & elem : table)
-                        visit(attrs.alloc(elem.first), elem.second);
+            v.mkAttrs(attrs);
+        } break;
+            ;
+        case toml::value_t::array: {
+            auto array = toml::get<std::vector<toml::value>>(t);
 
-                    v.mkAttrs(attrs);
-                }
-                break;;
-            case toml::value_t::array:
-                {
-                    auto array = toml::get<std::vector<toml::value>>(t);
-
-                    size_t size = array.size();
-                    v = state.ctx.mem.newList(size);
-                    for (size_t i = 0; i < size; ++i)
-                        visit(*(v.listElems()[i] = state.ctx.mem.allocValue()), array[i]);
-                }
-                break;;
-            case toml::value_t::boolean:
-                v.mkBool(toml::get<bool>(t));
-                break;;
-            case toml::value_t::integer:
-                v.mkInt(toml::get<int64_t>(t));
-                break;;
-            case toml::value_t::floating:
-                v.mkFloat(toml::get<NixFloat>(t));
-                break;;
-            case toml::value_t::string:
-                v.mkString(toml::get<std::string>(t));
-                break;;
-            case toml::value_t::local_datetime:
-            case toml::value_t::offset_datetime:
-            case toml::value_t::local_date:
-            case toml::value_t::local_time:
-                {
-                    if (experimentalFeatureSettings.isEnabled(Xp::ParseTomlTimestamps)) {
-                        auto attrs = state.ctx.buildBindings(2);
-                        attrs.alloc("_type").mkString("timestamp");
-                        std::ostringstream s;
-                        s << t;
-                        attrs.alloc("value").mkString(s.str());
-                        v.mkAttrs(attrs);
-                    } else {
-                        // NOLINTNEXTLINE(lix-foreign-exceptions)
-                        throw std::runtime_error("Dates and times are not supported");
-                    }
-                }
-                break;;
-            case toml::value_t::empty:
-                v.mkNull();
-                break;;
-
+            size_t size = array.size();
+            v = state.ctx.mem.newList(size);
+            for (size_t i = 0; i < size; ++i) {
+                visit(*(v.listElems()[i] = state.ctx.mem.allocValue()), array[i]);
+            }
+        } break;
+            ;
+        case toml::value_t::boolean:
+            v.mkBool(toml::get<bool>(t));
+            break;
+            ;
+        case toml::value_t::integer:
+            v.mkInt(toml::get<int64_t>(t));
+            break;
+            ;
+        case toml::value_t::floating:
+            v.mkFloat(toml::get<NixFloat>(t));
+            break;
+            ;
+        case toml::value_t::string:
+            v.mkString(toml::get<std::string>(t));
+            break;
+            ;
+        case toml::value_t::local_datetime:
+        case toml::value_t::offset_datetime:
+        case toml::value_t::local_date:
+        case toml::value_t::local_time: {
+            if (experimentalFeatureSettings.isEnabled(Xp::ParseTomlTimestamps)) {
+                auto attrs = state.ctx.buildBindings(2);
+                attrs.alloc("_type").mkString("timestamp");
+                std::ostringstream s;
+                s << t;
+                attrs.alloc("value").mkString(s.str());
+                v.mkAttrs(attrs);
+            } else {
+                // NOLINTNEXTLINE(lix-foreign-exceptions)
+                throw std::runtime_error("Dates and times are not supported");
+            }
+        } break;
+            ;
+        case toml::value_t::empty:
+            v.mkNull();
+            break;
+            ;
         }
     };
 
