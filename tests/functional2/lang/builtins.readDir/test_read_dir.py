4aedfd346fc341854cb2024a8b732da5e0c7d519
piegames <git@piegames.de>
2025-10-05 21:43:37 +0200

    tests/functional2/lang: Migrate builtins.readDir
    Change-Id: Ic70e4ef7780281f403d0421d63ecf849e083c01e

---
+++ tests/functional2/lang/builtins.readDir/test_read_dir.py
@@ -0,0 +1,29 @@
+from collections.abc import Callable
+from pathlib import Path
+
+from functional2.lang.test_lang import test_eval as nix_eval
+from functional2.testlib.fixtures.file_helper import (
+    with_files,
+    CopyFile,
+    Symlink,
+    AssetSymlink,
+    File,
+)
+from functional2.testlib.fixtures.nix import Nix
+from functional2.testlib.fixtures.snapshot import Snapshot
+
+
+@with_files(
+    {
+        "readDir": {
+            "foo": {},
+            "ldir": Symlink("./foo"),
+            "bar": File(""),
+            "linked": Symlink("./bar"),
+        },
+        "in.nix": CopyFile("in.nix"),
+        "out.exp": AssetSymlink("eval-okay.out.exp"),
+    }
+)
+def test_read_dir(files: Path, nix: Nix, snapshot: Callable[[str], Snapshot]):
+    nix_eval(files, nix, [], snapshot)
