cc5c62ad72f99273e352b43c63955e246e8432ac
eldritch horrors <pennae@lix.systems>
2025-12-22 23:31:06 +0100

    testing: migrate daemon-trust test
    Change-Id: I65fcdbe8f9d2f5ca4654341ed4a8878ef67b841a

---
+++ tests/functional2/daemon/test_trust.py
@@ -0,0 +1,21 @@
+import pytest
+import json
+
+from functional2.testlib.fixtures.nix import Nix
+
+
+@pytest.mark.parametrize(
+    ("trusted", "flags", "expected"),
+    [
+        ([], ["--default-trust"], False),
+        ([], ["--force-trusted"], True),
+        (["*"], ["--default-trust"], True),
+        (["*"], ["--force-untrusted"], False),
+    ],
+)
+def test_trust(nix: Nix, trusted: list[str], flags: list[str], expected: bool):
+    nix.settings.feature("nix-command", "daemon-trust-override")
+
+    with nix.daemon(flags, settings={"trusted-users": trusted}) as inner:
+        trusted = json.loads(inner.nix(["store", "ping", "--json"]).run().ok().stdout)
+        assert trusted["trusted"] == expected
