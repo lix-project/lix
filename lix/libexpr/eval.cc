c6f3f3a0d381fa592073d9bdb9b4673ee2d3d53e
eldritch horrors <pennae@lix.systems>
2026-01-14 22:29:49 +0100

    libexpr: fix app chain extension
    during the value rewrite we accidentally broke extension of incomplete
    primop application. this only shows up when binding on incomplete call
    to a primop to a name, binding an incomplete call to *that* to another
    name, and then finally calling the second binding with enough args for
    a complete primop application. since this only shows up when calling a
    primop with three or more args it took a while to surface. we have few
    builtins that match this: foldl', replaceStrings, and substring. these
    are not used incompletely in this manner very often, so this lingered.
    
    fixes #1102
    
    Change-Id: I218dffc14ae876efc86a86c7eb6c895e2405201c

--- lix/libexpr/eval.cc
+++ lix/libexpr/eval.cc
@@ -1639,7 +1639,14 @@ void EvalState::callFunction(Value & fun, std::span<Value> args, Value & vRes, c
 
     Value vCur(fun);
 
-    auto makeAppChain = [&]() { vRes = {NewValueAs::app, ctx.mem, vCur, args}; };
+    auto makeAppChain = [&]() {
+        if (vCur.isApp()) {
+            auto & app = vCur.app();
+            vRes = {NewValueAs::app, ctx.mem, app.left(), app.args(), args};
+        } else {
+            vRes = {NewValueAs::app, ctx.mem, vCur, args};
+        }
+    };
 
     const Attr * functor;
 
