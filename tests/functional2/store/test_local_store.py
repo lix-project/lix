b8b69dde050583ba89f3dc0330099241aaadc4a5
eldritch horrors <pennae@lix.systems>
2026-02-26 20:30:45 +0100

    testing: migrate local-store.sh
    Change-Id: I5172fc2611a56b19f55d5c05dacdfef5cf330739

---
+++ tests/functional2/store/test_local_store.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import pytest
+
+from testlib.fixtures.nix import Nix
+from testlib.fixtures.file_helper import File, with_files
+
+
+@with_files({"example.txt": File("example")})
+class TestLocalStore:
+    @pytest.fixture(autouse=True)
+    def setup(self, nix: Nix, files: Path):  # noqa: ARG002
+        nix.settings.add_xp_feature("nix-command")
+
+        store = nix.env.dirs.home / "x"
+        store.mkdir()
+        nix.env.dirs.nix_store_dir = store
+        nix.env.dirs.real_store_dir = store
+
+        self.path = (
+            nix.nix_store(["--store", "./x", "--add", "example.txt"]).run().ok().stdout_plain
+        )
+
+    def test_path_info_relative(self, nix: Nix):
+        store = "./x"
+        path = nix.nix(["path-info", "--store", store, self.path]).run().ok().stdout_plain
+        assert path == self.path
+
+    def test_path_info_absolute(self, nix: Nix):
+        store = f"{nix.env.dirs.home}/x"
+        path = nix.nix(["path-info", "--store", store, self.path]).run().ok().stdout_plain
+        assert path == self.path
+
+    def test_path_info_uri(self, nix: Nix):
+        store = f"local?root={nix.env.dirs.home}/x"
+        path = nix.nix(["path-info", "--store", store, self.path]).run().ok().stdout_plain
+        assert path == self.path
+
+    def test_local_is_trusted(self, nix: Nix):
+        info = nix.nix(["--store", "./x", "store", "ping", "--json"]).run().json()
+        assert info["trusted"]
+
+    def test_doctor_shows_trust(self, nix: Nix):
+        # doctor wants nix-env on path, and the profile lookup test will *always* fail
+        result = nix.nix(["--store", "./x", "doctor"]).run().expect(2)
+        assert "You are trusted by" in result.stderr_plain
