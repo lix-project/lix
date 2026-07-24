83bbcb39dcd84d5297d72fbd40b9978b5ffb01d9
eldritch horrors <pennae@lix.systems>
2026-07-24 16:43:15 +0200

    lix-rs: add missing build script dep to generated.rs
    without this cargo would not rerun if only the zngur spec changed. oops.
    
    Change-Id: I671ab3a103f2ae2f158e49230ce5e268e706689a

--- lix/lix-rs/build.rs
+++ lix/lix-rs/build.rs
@@ -4,15 +4,16 @@ fn main() {
     // this is a hack to let rust-analyzer find the generated code. we could
     // also include! from MESON_BUILD_DIR directly, but rust-analyzer cannot
     // be told to look there. not with any method we came across yet anyway.
+    let generated = format!(
+        "{}/lix/lix-rs/main.zng.rs",
+        env::var("MESON_BUILD_DIR").expect("MESON_BUILD_DIR")
+    );
     fs::write(
         format!("{}/generated.rs", env::var("OUT_DIR").expect("OUT_DIR")),
-        fs::read(format!(
-            "{}/lix/lix-rs/main.zng.rs",
-            env::var("MESON_BUILD_DIR").expect("MESON_BUILD_DIR")
-        ))
-        .expect("read main.zng.rs"),
+        fs::read(&generated).expect("read main.zng.rs"),
     )
     .expect("write generated.rs");
+    println!("cargo::rerun-if-changed={generated}");
 
     println!("cargo::rerun-if-env-changed=CXX_LINK_DIR_FOR_TEST");
     if let Some(dir) = env::var("CXX_LINK_DIR_FOR_TEST").ok() {
