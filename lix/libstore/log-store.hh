8fa088a1f19826a4993425c8fec08f57bed5c8ff
eldritch horrors <pennae@lix.systems>
2026-09-16 14:03:09 +0200

    libstore: remove stray LogStore::require
    Change-Id: Ib4dc5a2bcf2e0207d930acff2a2a0407fa3ce4db

--- lix/libstore/log-store.hh
+++ lix/libstore/log-store.hh
@@ -19,8 +19,6 @@ struct LogStore : public virtual Store
     virtual kj::Promise<Result<std::optional<std::string>>> getBuildLogExact(const StorePath & path) = 0;
 
     virtual kj::Promise<Result<void>> addBuildLog(const StorePath & path, std::string_view log) = 0;
-
-    static LogStore & require(Store & store);
 };
 
 }
